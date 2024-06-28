079f77dacc5d46fd6bdd7617a6a0c6dad4db73a2
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-06-29 01:48:14 +0900

    feat: next.config.mjs에 next-image 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withImages from "next-images";
+
+const nextConfig = withImages({
+  compiler: {
+    styledComponents: true,
+  },
+});
 
 export default nextConfig;
