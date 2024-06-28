8a5ef4ed1dc3d5d71e37e03804b3f860ecf68cba
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-06-29 01:55:59 +0900

    refactor: Card 컴포넌트 수정

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -16,12 +16,16 @@ interface Props {
   };
 }
 
+type priceType = "종가" | "고가" | "저가";
+
 const Card = ({
   onClick,
   clubName,
   clubImage = "/images/minoru.png",
   price,
 }: Props) => {
+  const priceList: priceType[] = ["종가", "고가", "저가"];
+
   return (
     <S.CardBox onClick={onClick}>
       <S.Name>{clubName}</S.Name>
@@ -37,17 +41,11 @@ const Card = ({
         $rowGap={"7px"}
         $padding={"13px 17px"}
       >
-        <S.Text $priceType={"종가"}>
-          종가 <span>{price.종가}</span>
-        </S.Text>
-
-        <S.Text $priceType={"고가"}>
-          고가 <span>{price.고가}</span>
-        </S.Text>
-
-        <S.Text $priceType={"저가"}>
-          저가 <span>{price.저가}</span>
-        </S.Text>
+        {priceList.map((item, idx) => (
+          <S.Text key={idx} $priceType={item}>
+            {item} <span>{price[item]}</span>
+          </S.Text>
+        ))}
       </Column>
     </S.CardBox>
   );
