0748c8cc73e6921479f41baacae69d7cccff22b9
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-06-29 01:45:59 +0900

    feat: 더미데이터 추가

---
+++ src/constants/dummy.constant.ts
@@ -0,0 +1,236 @@
+export const REGISTED_CLUB_STOKE_ITMES = [
+  {
+    clubName: "B1ND",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "CNS",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "DUCAMI",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "ALT",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "DLC",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "모디",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "inD",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "Connect",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "8bit",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "삼디",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+];
+
+export const INQUIRED_CLUB_STOKE_ITEMS = [
+  {
+    clubName: "B1ND",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "CNS",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "DUCAMI",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "ALT",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "DLC",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+];
+
+export const INCREASE_CLUB_STOKE_ITMES = [
+  {
+    clubName: "B1ND",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "CNS",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "DUCAMI",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "ALT",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "DLC",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+];
+
+export const DECREASE_CLUB_STOKE_ITMES = [
+  {
+    clubName: "B1ND",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "CNS",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "DUCAMI",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "ALT",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+  {
+    clubName: "DLC",
+    clubImage: "/images/minoru.png",
+    price: {
+      종가: "87,849,000",
+      고가: "88,249,000",
+      저가: "87,630,000",
+    },
+  },
+];
