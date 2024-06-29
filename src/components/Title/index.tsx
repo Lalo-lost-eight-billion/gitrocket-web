4069d263cf1a162ae1b801857b5d31b1a7ec649b
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-06-30 04:31:22 +0900

    refactor: 타이틀 컴포넌트 수정

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,14 +1,27 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { CSSProperties } from "styled-components";
 
-const Title = ({ content }: { content: string }) => {
-  return <StyledTitle>{content}</StyledTitle>;
+interface Props {
+  content: string;
+  size?: CSSProperties["fontSize"];
+  color?: CSSProperties["color"];
+}
+
+const Title = ({ content, size = "20px", color = "#21242c" }: Props) => {
+  return (
+    <StyledTitle $size={size} $color={color}>
+      {content}
+    </StyledTitle>
+  );
 };
 
 export default Title;
 
-const StyledTitle = styled.p`
-  font-size: 20px;
-  color: #21242c;
+const StyledTitle = styled.p<{
+  $size: CSSProperties["fontSize"];
+  $color: CSSProperties["color"];
+}>`
+  font-size: ${({ $size }) => $size};
+  color: ${({ $color }) => $color};
   font-family: "Pretendard-Bold" !important;
 `;
